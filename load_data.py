4090c75c7d3a37b661ce313be183f9340edab23d
yjiao1213 <36117600+yjiao1213@users.noreply.github.com>
2018-11-26 15:21:25 -0500

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -1,54 +1,75 @@
 import os
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import cross_validate
-
-target_name = "clean_target03.csv"
-data_name = "pca_result.csv"
-
-pwd = os.getcwd()
-target = pd.read_csv(os.path.join(pwd, "data", target_name)).values.ravel()
-data = pd.read_csv(os.path.join(pwd, "data", data_name)).values
-
-print(np.count_nonzero(target))
-print(data.shape)
-print(target.shape)
 
 
-# train_x = data[:int(4*data.shape[0]/5), :]
-# train_y = target[:int(4*target.shape[0]/5)]
+# # using PCA data
+# target_name = "clean_target03.csv"
+# data_name = "pca_result.csv"
 #
-# test_x = data[int(4*data.shape[0]/5):, :]
-# test_y_label = target[int(4*target.shape[0]/5):]
+# pwd = os.getcwd()
+# target = pd.read_csv(os.path.join(pwd, "data", target_name)).values.ravel()
+# data = pd.read_csv(os.path.join(pwd, "data", data_name)).values
+#
+# print(np.count_nonzero(target))
+# print(data.shape)
+# print(target.shape)
+#
+#
+# from sklearn.model_selection import cross_val_score
+#
+# # DT
+# from sklearn import tree
+# clf_dt = tree.DecisionTreeClassifier()
+# scores = cross_val_score(clf_dt, data, target, cv=5)
+# print("Accuracy of DT: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+#
+# # random froset
+# from sklearn.ensemble import RandomForestClassifier
+# clf_rf = RandomForestClassifier(n_estimators=100)
+# scores = cross_val_score(clf_rf, data, target, cv=5)
+# print("Accuracy of random forest: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+#
+# # SVM
+# from sklearn import svm
+# clf_svm = svm.SVC(gamma='scale')
+# scores = cross_val_score(clf_rf, data, target, cv=5)
+# print("Accuracy of SVM: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+
+
+# using RAW data
+file = "clean_result02.csv"
+pwd = os.getcwd()
+trainFile = os.path.join(pwd, "data", file)
+trainData = pd.read_csv(trainFile)
+train_matrix= trainData.values
+#train_matrix = np.delete(train_matrix, 0, axis=1)
+
+y = train_matrix[:,10]
+x = np.delete(train_matrix, 10, axis=1)
+
 
 from sklearn.model_selection import cross_val_score
 
 # DT
 from sklearn import tree
 clf_dt = tree.DecisionTreeClassifier()
-scores = cross_val_score(clf_dt, data, target, cv=5)
+scores = cross_val_score(clf_dt, x, y, cv=5)
 print("Accuracy of DT: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 # random froset
 from sklearn.ensemble import RandomForestClassifier
 clf_rf = RandomForestClassifier(n_estimators=100)
-scores = cross_val_score(clf_rf, data, target, cv=5)
+scores = cross_val_score(clf_rf, x, y, cv=5)
 print("Accuracy of random forest: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 # SVM
 from sklearn import svm
 clf_svm = svm.SVC(gamma='scale')
-scores = cross_val_score(clf_rf, data, target, cv=5)
+scores = cross_val_score(clf_rf, x, y, cv=5)
 print("Accuracy of SVM: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-#scoring = ['precision_macro', 'recall_macro']
-
-
-
-
-#
-# print(scores['test_recall_macro'])
-#print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
 #
