ea493038222fbc8dfe27d29071100c476e570396
Wade <liuweixi0819@gmail.com>
2018-12-12 10:17:35 -0500

    update

--- output/final_version.py
+++ output/final_version.py
@@ -6,9 +6,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import StratifiedKFold
 import matplotlib.pyplot as plt
 from sklearn import tree, svm
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.linear_model import LogisticRegression
 from output.problem2 import *
 
 def load_data():
@@ -36,26 +37,33 @@ def knn_classification(X, Y, k_range, cv):
     '''
     train_errors = list()
     test_errors = list()
+    specificities = list()
     for i in k_range:
         print("start train N = ", i)
         train_error = list()
         test_error = list()
+        specificity = list()
         clf_knn = KNeighborsClassifier(n_neighbors=i)
         for train, test in cv.split(X, Y):
-            print(X[train].shape)
-            print(Y[train].shape)
             clf_knn.fit(X[train], Y[train])
             train_error.append(clf_knn.score(X[train], Y[train]))
             test_error.append(clf_knn.score(X[test], Y[test]))
+            predictions = clf_knn.predict(X[test])
+            cm = confusion_matrix(Y[test], predictions)
+            specificity.append(cm[1,1]/(cm[1,1] + cm[1,0]))
         train_errors.append(sum(train_error)/len(train_error))
         test_errors.append(sum(test_error)/len(test_error))
+        specificities.append(sum(specificity)/len(specificity))
 
+    print("test accuracy:", test_errors)
+    print("test specificity:", specificities)
     plt.figure()
     plt.plot(k_range, train_errors, label='Train')
     plt.plot(k_range, test_errors, label='Test')
+    plt.plot(k_range, specificities, label='specificity(true negative rate)')
     plt.legend(loc='lower left')
-    plt.ylim([0, 1.1])
-    plt.xlim([0, 21])
+    #plt.ylim([0, 1.1])
+    #plt.xlim([0, 21])
     plt.title('Evaluation of KNN model')
     plt.xlabel('The number of N')
     plt.ylabel('Performance')
@@ -65,29 +73,36 @@ def knn_classification(X, Y, k_range, cv):
 def dt_classification(X, Y, cv):
     '''
     decision tree model
-
     '''
     train_errors = list()
     test_errors = list()
-    depths = range(5, 30)
-    #depths = range(X.shape[1]-10, X.shape[1])
+    specificities = list()
+    depths = range(1, 30, 2)
     for depth in depths:
         train_error = list()
         test_error = list()
+        specificity = list()
         clf_dt = tree.DecisionTreeClassifier(max_depth=depth)
         for train, test in cv.split(X, Y):
             clf_dt.fit(X[train], Y[train])
             train_error.append(clf_dt.score(X[train], Y[train]))
             test_error.append(clf_dt.score(X[test], Y[test]))
+            predictions = clf_dt.predict(X[test])
+            cm = confusion_matrix(Y[test], predictions)
+            specificity.append(cm[1,1]/(cm[1,1] + cm[1,0]))
+        specificities.append(sum(specificity)/len(specificity))
         train_errors.append(sum(train_error)/len(train_error))
         test_errors.append(sum(test_error)/len(test_error))
 
+    print("test accuracy:", test_errors)
+    print("test specificity:", specificities)
     plt.figure()
     plt.plot(depths, train_errors, label='Train')
     plt.plot(depths, test_errors, label='Test')
+    plt.plot(depths, specificities, label='specificity')
     plt.legend(loc='lower left')
-    plt.ylim([0, 1.1])
-    plt.xlim([depths[0]-2, depths[-1]+2])
+    #plt.ylim([0, 1.1])
+    #plt.xlim([depths[0]-2, depths[-1]+2])
     plt.title('Evaluation of DT model')
     plt.xlabel('The depth')
     plt.ylabel('Performance')
@@ -97,23 +112,33 @@ def dt_classification(X, Y, cv):
 def rf_classification(X, Y, cv):
     train_errors = list()
     test_errors = list()
-    tree_nums = range(5, 100, 5)
+    specificities = list()
+    tree_nums = range(100, 2000, 100)
     for tree_num in tree_nums:
         train_error = list()
         test_error = list()
+        specificity = list()
         clf_rf = RandomForestClassifier(n_estimators=tree_num)
         for train, test in cv.split(X, Y):
             clf_rf.fit(X[train], Y[train])
             train_error.append(clf_rf.score(X[train], Y[train]))
             test_error.append(clf_rf.score(X[test], Y[test]))
+            predictions = clf_rf.predict(X[test])
+            cm = confusion_matrix(Y[test], predictions)
+            specificity.append(cm[1,1]/(cm[1,1] + cm[1,0]))
+        specificities.append(sum(specificity)/len(specificity))
         train_errors.append(sum(train_error)/len(train_error))
         test_errors.append(sum(test_error)/len(test_error))
+
+    print("test accuracy:", test_errors)
+    print("test specificity:", specificities)
     plt.figure()
+    plt.plot(tree_nums, specificities, label='specificity')
     plt.plot(tree_nums, train_errors, label='Train')
     plt.plot(tree_nums, test_errors, label='Test')
     plt.legend(loc='lower left')
-    plt.ylim([0, 1.1])
-    plt.xlim([0, 102])
+    #plt.ylim([0, 1.1])
+    #plt.xlim([0, 102])
     plt.title('Evaluation of random forest model')
     plt.xlabel('The number of trees')
     plt.ylabel('Performance')
@@ -121,44 +146,59 @@ def rf_classification(X, Y, cv):
 
 
 def svm_classification(X, Y, cv):
-    kernels = ['poly', 'rbf', 'sigmoid']
-    x = [1, 2, 3, 4]
+    # kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+    # x = [1, 2, 3, 4]
+    kernels = ['linear']
+    x = [1]
     train_errors = list()
     test_errors = list()
+    specificities = list()
     for kernel_unit in kernels:
         train_error = list()
         test_error = list()
+        specificity = list()
         clf_svm = svm.SVC(kernel=kernel_unit, probability=True, gamma='scale')
         for train, test in cv.split(X, Y):
+            print("Here")
             clf_svm.fit(X[train], Y[train])
             train_error.append(clf_svm.score(X[train], Y[train]))
             test_error.append(clf_svm.score(X[test], Y[test]))
+            predictions = clf_svm.predict(X[test])
+            cm = confusion_matrix(Y[test], predictions)
+            specificity.append(cm[1, 1] / (cm[1, 1] + cm[1, 0]))
+        specificities.append(sum(specificity) / len(specificity))
         print("the kernel is ", kernel_unit)
         print("the accuracy of train is ", train_error)
         print("the accuracy of test is ", test_error)
         train_errors.append(sum(train_error)/len(train_error))
         test_errors.append(sum(test_error)/len(test_error))
+
+    print("test accuracy:", test_errors)
+    print("test specificity:", specificities)
     plt.figure()
+    plt.scatter(x, specificities, label='specificity')
     plt.scatter(x, train_errors, label='Train')
     plt.scatter(x, test_errors, label='Test')
     plt.xticks(x, kernels, rotation=45)
     plt.legend(loc='lower left')
-    plt.ylim([0, 1.1])
-    plt.xlim([0, 5])
+    #plt.ylim([0, 1.1])
+    #plt.xlim([0, 5])
     plt.title('Evaluation of SVM model')
     plt.xlabel('The Kernel')
     plt.ylabel('Performance')
     plt.show()
 
+
+
 def ANN_classification(X, Y, cv):
-    # train_errors = list()
+    train_accuracies = list()
     test_accuracies = list()
     specificities = list()
     x_axis= []
-    for i in range(1, 11):
+    for i in range(1, 51):
         x_axis.append(0.0001 * (i * 2))
         print("start train N = ", i)
-        train_error = list()
+        train_accuracy = list()
         test_accuracy = list()
         specificity = list()
         for train, test in cv.split(X, Y):
@@ -167,23 +207,25 @@ def ANN_classification(X, Y, cv):
             Y_train = Y[train]
             Y_test = Y[test]
             W1, b1, W2, b2 = ann_train(X_train, Y_train, alpha=0.0001*(i * 2), n_epoch=10)
+            Y, P = ann_predict(X_train, W1, b1, W2, b2)
             Y_predict, P = ann_predict(X_test, W1, b1, W2, b2)
+            cm_train = confusion_matrix(Y_train, Y)
             cm = confusion_matrix(Y_test, Y_predict)
+            train_accuracy.append((cm_train[1, 1] + cm_train[0, 0]) / (cm_train[0, 0] + cm_train[0, 1] + cm_train[1, 1] + cm_train[1, 0]))
             test_accuracy.append((cm[1, 1] + cm[0, 0]) / (cm[0, 0] + cm[0, 1] + cm[1, 1] + cm[1, 0]))
             specificity.append(cm[1, 1] / (cm[1, 1] + cm[1, 0]))
-        # train_errors.append(sum(train_error) / len(train_error))
+        train_accuracies.append(sum(train_accuracy) / len(train_accuracy))
         test_accuracies.append(sum(test_accuracy) / len(test_accuracy))
         specificities.append(sum(specificity) / len(specificity))
     plt.figure()
+    plt.plot(x_axis, train_accuracies, label='Train')
     plt.plot(x_axis, test_accuracies, label='Test')
     plt.plot(x_axis, specificities, label='specificity')
     plt.legend(loc='lower left')
     plt.title('Evaluation of ANN model')
-    plt.xlabel('The number of alpha')
+    plt.xlabel('Alpha')
     plt.ylabel('Performance')
     plt.show()
-
-
 
 # def NN_classification(X, Y, cv):
 #     clf_NN = MLPClassifier(hidden_layer_sizes=(9, 9, 9,),
@@ -209,22 +251,63 @@ def ANN_classification(X, Y, cv):
 #     print(clf_NN.score(X_test, Y_test))
 
 
+def Ada_classification(X, Y, cv):
+    clf_dt = tree.DecisionTreeClassifier()
+    n_ests = range(100, 500, 50)
+    train_errors = list()
+    test_errors = list()
+    specificities = list()
+    for n_est in n_ests:
+        bdt = AdaBoostClassifier(clf_dt, n_estimators=n_est)
+        train_error = list()
+        test_error = list()
+        specificity = list()
+        for train, test in cv.split(X, Y):
+            bdt.fit(X[train], Y[train])
+            train_error.append(bdt.score(X[train], Y[train]))
+            test_error.append(bdt.score(X[test], Y[test]))
+            predictions = bdt.predict(X[test])
+            cm = confusion_matrix(Y[test], predictions)
+            specificity.append(cm[1, 1] / (cm[1, 1] + cm[1, 0]))
+        specificities.append(sum(specificity) / len(specificity))
+        train_errors.append(sum(train_error)/len(train_error))
+        test_errors.append(sum(test_error)/len(test_error))
+    print("test accuracy:", test_errors)
+    print("test specificity:", specificities)
+
+    print("test accuracy:", test_errors)
+    print("test specificity:", specificities)
+    plt.figure()
+    plt.plot(n_ests, specificities, label='specificity')
+    plt.plot(n_ests, train_errors, label='Train')
+    plt.plot(n_ests, test_errors, label='Test')
+
+    plt.legend(loc='lower left')
+    #plt.ylim([0.5, 1.1])
+    #plt.xlim([90, 510])
+    plt.title('Evaluation of Adaboost with DT model')
+    plt.xlabel('The estimators')
+    plt.ylabel('Performance')
+    plt.show()
+
+
 if __name__ == "__main__":
 
     X, Y, X_pca, Y_pca = load_data()
     cv = StratifiedKFold(n_splits=2)
+
     ## after pca
-    k_range = range(1, 20, 2)
+    k_range = range(1, 35, 2)
     knn_classification(X_pca, Y_pca, k_range, cv)
     dt_classification(X_pca, Y_pca, cv)
     rf_classification(X_pca, Y_pca, cv)
-    #svm_classification(X_pca, Y_pca, cv)
-    #ANN_classification(X_pca, Y_pca, cv)
+    svm_classification(X_pca, Y_pca, cv)
+    ANN_classification(X_pca, Y_pca, cv)
 
     ## before pca
-    # k_range = range(1, 20, 2)
-    # knn_classification(X, Y, k_range, cv)
-    # dt_classification(X, Y, cv)
-    # rf_classification(X_pca, Y_pca, cv)
-    # svm_classification(X, Y, cv)
-    # ANN_classification(X, Y, cv)
+    k_range = range(1, 35, 2)
+    knn_classification(X, Y, k_range, cv)
+    dt_classification(X, Y, cv)
+    rf_classification(X, Y, cv)
+    svm_classification(X, Y, cv)
+    ANN_classification(X, Y, cv)
