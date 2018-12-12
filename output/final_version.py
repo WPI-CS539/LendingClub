7e409318c2816b3e4f5994cabb2d45930ab248a3
Wade <liuweixi0819@gmail.com>
2018-12-12 01:18:30 -0500

    update

--- output/final_version.py
+++ output/final_version.py
@@ -9,6 +9,7 @@ from sklearn import tree, svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix, classification_report
+from output.problem2 import *
 
 def load_data():
     '''
@@ -120,7 +121,7 @@ def rf_classification(X, Y, cv):
 
 
 def svm_classification(X, Y, cv):
-    kernels = [ 'linear','poly', 'rbf', 'sigmoid']
+    kernels = ['poly', 'rbf', 'sigmoid']
     x = [1, 2, 3, 4]
     train_errors = list()
     test_errors = list()
@@ -149,39 +150,81 @@ def svm_classification(X, Y, cv):
     plt.ylabel('Performance')
     plt.show()
 
+def ANN_classification(X, Y, cv):
+    # train_errors = list()
+    test_accuracies = list()
+    specificities = list()
+    x_axis= []
+    for i in range(1, 11):
+        x_axis.append(0.0001 * (i * 2))
+        print("start train N = ", i)
+        train_error = list()
+        test_accuracy = list()
+        specificity = list()
+        for train, test in cv.split(X, Y):
+            X_train = np.asmatrix(X[train])
+            X_test = np.asmatrix(X[test])
+            Y_train = Y[train]
+            Y_test = Y[test]
+            W1, b1, W2, b2 = ann_train(X_train, Y_train, alpha=0.0001*(i * 2), n_epoch=10)
+            Y_predict, P = ann_predict(X_test, W1, b1, W2, b2)
+            cm = confusion_matrix(Y_test, Y_predict)
+            test_accuracy.append((cm[1, 1] + cm[0, 0]) / (cm[0, 0] + cm[0, 1] + cm[1, 1] + cm[1, 0]))
+            specificity.append(cm[1, 1] / (cm[1, 1] + cm[1, 0]))
+        # train_errors.append(sum(train_error) / len(train_error))
+        test_accuracies.append(sum(test_accuracy) / len(test_accuracy))
+        specificities.append(sum(specificity) / len(specificity))
+    plt.figure()
+    plt.plot(x_axis, test_accuracies, label='Test')
+    plt.plot(x_axis, specificities, label='specificity')
+    plt.legend(loc='lower left')
+    plt.title('Evaluation of ANN model')
+    plt.xlabel('The number of alpha')
+    plt.ylabel('Performance')
+    plt.show()
 
-def NN_classification(X, Y, cv):
-    clf_NN = MLPClassifier(hidden_layer_sizes=(9, 9, 9,),
-                  activation='tanh', batch_size='auto',
-                  learning_rate ='constant', learning_rate_init = 0.001,
-                  max_iter = 200,
-                  shuffle = True,
-                  verbose = True,
-                  warm_start = False,
-                  early_stopping = True, n_iter_no_change = 30, validation_fraction = 0.1,
-                  beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08)
 
-    X_test = X[20000:25000,:]
-    Y_test = Y[20000:25000]
-    X = X[0:20000,:]
-    Y = Y[0:20000]
-    print(X.shape)
-    print(Y.shape)
-    clf_NN.fit(X, Y)
-    predictions = clf_NN.predict(X_test)
-    print(confusion_matrix(Y_test, predictions))
-    print(classification_report(Y_test, predictions))
-    print(clf_NN.score(X_test, Y_test))
+
+# def NN_classification(X, Y, cv):
+#     clf_NN = MLPClassifier(hidden_layer_sizes=(9, 9, 9,),
+#                   activation='tanh', batch_size='auto',
+#                   learning_rate ='constant', learning_rate_init = 0.001,
+#                   max_iter = 200,
+#                   shuffle = True,
+#                   verbose = True,
+#                   warm_start = False,
+#                   early_stopping = True, n_iter_no_change = 30, validation_fraction = 0.1,
+#                   beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08)
+#
+#     X_test = X[20000:25000,:]
+#     Y_test = Y[20000:25000]
+#     X = X[0:20000,:]
+#     Y = Y[0:20000]
+#     print(X.shape)
+#     print(Y.shape)
+#     clf_NN.fit(X, Y)
+#     predictions = clf_NN.predict(X_test)
+#     print(confusion_matrix(Y_test, predictions))
+#     print(classification_report(Y_test, predictions))
+#     print(clf_NN.score(X_test, Y_test))
 
 
 if __name__ == "__main__":
 
     X, Y, X_pca, Y_pca = load_data()
-
     cv = StratifiedKFold(n_splits=2)
+    ## after pca
+    k_range = range(1, 20, 2)
+    knn_classification(X_pca, Y_pca, k_range, cv)
+    dt_classification(X_pca, Y_pca, cv)
+    rf_classification(X_pca, Y_pca, cv)
+    #svm_classification(X_pca, Y_pca, cv)
+    #ANN_classification(X_pca, Y_pca, cv)
+
+    ## before pca
     # k_range = range(1, 20, 2)
-    #knn_classification(X_pca, Y_pca, k_range, cv)
-    #dt_classification(X_pca, Y_pca, cv)
-    # #rf_classification(X_pca, Y_pca, cv)
-    # svm_classification(X_pca, Y_pca, cv)
-    NN_classification(X_pca, Y_pca, cv)
+    # knn_classification(X, Y, k_range, cv)
+    # dt_classification(X, Y, cv)
+    # rf_classification(X_pca, Y_pca, cv)
+    # svm_classification(X, Y, cv)
+    # ANN_classification(X, Y, cv)
